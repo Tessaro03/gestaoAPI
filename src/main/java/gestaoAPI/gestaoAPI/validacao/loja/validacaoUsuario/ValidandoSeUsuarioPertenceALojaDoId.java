294c4c5f19c2f08f25ecd3b2e0491d474bd8f107
Gabriel Tessaro Favareto <119542621+Tessaro03@users.noreply.github.com>
2024-04-01 16:33:52 -0300

    Att 0.9.1

--- src/main/java/gestaoAPI/gestaoAPI/validacao/loja/validacaoUsuario/ValidandoSeUsuarioPertenceALojaDoId.java
+++ src/main/java/gestaoAPI/gestaoAPI/validacao/loja/validacaoUsuario/ValidandoSeUsuarioPertenceALojaDoId.java
@@ -1,11 +1,13 @@
 package gestaoAPI.gestaoAPI.validacao.loja.validacaoUsuario;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import gestaoAPI.gestaoAPI.infra.exception.ValidacaoExcepetion;
 import gestaoAPI.gestaoAPI.infra.token.UsuarioToken;
 import jakarta.servlet.http.HttpServletRequest;
 
+@Service
 public class ValidandoSeUsuarioPertenceALojaDoId implements ValidadorLojaUsuario{
 
     @Autowired
